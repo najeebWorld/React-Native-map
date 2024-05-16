84cbef396eefc3bc2a1c7e5a65ab56304717e3ae
Najeeb Abdalla <najeeb040@gmail.com>
2024-05-16 11:27:57 +0300

    ss

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [showMap, setShowMap] = useState(false);
+
+	return (
+		<View style={styles.container}>
+			{showMap ? (
+				<View style={styles.mapContainer}>
+					<MapView style={styles.map} />
+					<Button
+						title="Back"
+						onPress={() => setShowMap(false)}
+						style={styles.backButton}
+					/>
+				</View>
+			) : (
+				<Button title="Show Map" onPress={() => setShowMap(true)} />
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	mapContainer: {
+		...StyleSheet.absoluteFillObject,
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
+	backButton: {
+		position: "absolute",
+		bottom: 20,
+		alignSelf: "center",
+	},
 });
